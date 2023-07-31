379765d3730a16f41f0400861ff49c09dd556d76
Mariusz Słowiński <mario.slowinski@gmail.com>
2023-07-31 21:10:12 +0200

    Remove "manager" data.

--- manager.vars.tf
+++
@@ -1,9 +0,0 @@
-variable "manager" {
-  type = object({
-    mail                = optional(string) # The SMTP address for the user.
-    mail_nickname       = optional(string) # The email alias of the user.
-    object_id           = optional(string) # The object ID of the user.
-    user_principal_name = optional(string) # The user principal name (UPN) of the user.
-  })
-  default = null
-}
