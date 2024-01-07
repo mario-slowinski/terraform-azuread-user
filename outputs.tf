582bd034f637f70128ed4899102ddc9847473630
Mariusz Słowiński <mario.slowinski@gmail.com>
2024-01-07 15:35:53 +0100

    Output Azure Key Vault Secret with user's password.

--- outputs.tf
+++ outputs.tf
@@ -9,3 +9,9 @@ output "password" {
   value       = coalesce(sensitive(var.password), random_password.user.result)
   sensitive   = true
 }
+
+output "secret" {
+  description = "Azure Key Vault Secret."
+  value       = { for name, secret in azurerm_key_vault_secret.name : name => secret }
+  sensitive   = true
+}
